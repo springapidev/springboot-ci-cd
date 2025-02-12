1f58684eeef036187c9f9a4cbedaba6d337fffdf
Mohammad Rajaul Islam <springapidev@gmail.com>
2025-02-13 01:05:58 +0600

    Initial Spring Boot app setup

---
+++ src/test/java/com/alltimeacademy/springbootcicd/SpringbootCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alltimeacademy.springbootcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
